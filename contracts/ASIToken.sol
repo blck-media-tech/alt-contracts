dc3426af3051e0736ce80fa39aeed863a4e1e9aa
Episqol <khomchykdmitry@gmail.com>
2023-01-12 20:59:41 +0300

    ALT-11 some corrected codestyle

--- contracts/ASIToken.sol
+++ contracts/ASIToken.sol
@@ -5,13 +5,13 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 
 contract ASIToken is ERC20Capped {
-    constructor(uint _initialSupply, uint256 _cap)
+    constructor(uint256 _initialSupply, uint256 _cap)
     ERC20("AltSignals", "ASI")
     ERC20Capped(_cap * 10**decimals()) {
         _mint(msg.sender, _initialSupply * 10**decimals());
     }
 
-    function burn(uint256 amount) public virtual {
-        _burn(msg.sender, amount);
+    function burn(uint256 _amount) public virtual {
+        _burn(msg.sender, _amount);
     }
 }
