12d73142bd3d690e1006ff368f3622e894bae299
Episqol <khomchykdmitry@gmail.com>
2023-01-12 13:56:19 +0300

    ALT-11 create basic ERC20 token

---
+++ contracts/ASIToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+
+contract ASIToken is ERC20Capped {
+    constructor(uint _initialSupply, uint256 _cap)
+    ERC20("AltSignals", "ASI")
+    ERC20Capped(_cap * 10**decimals()) {
+        _mint(msg.sender, _initialSupply * 10**decimals());
+    }
+}
