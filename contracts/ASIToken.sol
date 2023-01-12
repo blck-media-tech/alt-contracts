55578f54d8d829eb9f126a14d73c84eb1fca17e4
Episqol <khomchykdmitry@gmail.com>
2023-01-12 21:04:11 +0300

    ALT-11 implement Ownable, crete onlyOwner mint function

--- contracts/ASIToken.sol
+++ contracts/ASIToken.sol
@@ -3,15 +3,21 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract ASIToken is ERC20Capped {
+contract ASIToken is ERC20Capped, Ownable {
     constructor(uint256 _initialSupply, uint256 _cap)
     ERC20("AltSignals", "ASI")
-    ERC20Capped(_cap * 10**decimals()) {
+    ERC20Capped(_cap * 10**decimals())
+    Ownable() {
         _mint(msg.sender, _initialSupply * 10**decimals());
     }
 
     function burn(uint256 _amount) public virtual {
         _burn(msg.sender, _amount);
+    }
+
+    function mint(address _to, uint256 _amount) public virtual onlyOwner {
+        _mint(_to, _amount);
     }
 }
