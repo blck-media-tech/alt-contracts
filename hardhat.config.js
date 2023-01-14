df20bbaddd0d9d9fdfeea3fcd1168ddb6156b9dc
0xvidejk <videjk@yandex.ru>
2023-01-14 19:12:25 +0300

    ALT-30 - update hardhat.config.js configuration

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("./task/subtasks");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("./task/asiToken/asiToken.deploy");
+
 module.exports = {
     solidity: "0.8.17",
     mocha: {
         timeout: 5000,
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
+    networks: {
+        hardhat: {},
+        testnet: {
+            accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+            url: "https://goerli.infura.io/v3/",
+        },
+        mainnet: {
+            accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+            url: "https://eth.llamarpc.com",
+        },
     },
 };
